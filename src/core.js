df9392a074d70ed1053c06d6889a144410f77805
Ryan Burrows <rhburrows@gmail.com>
2011-06-13 23:48:17 -0700

    More extraction
    Similar to the display extract the input manager as it could (in
    theory) be handled in different ways.
    
    Note both this and the display need cleanup that is much more obvious
    now that they are extracted.

--- src/core.js
+++ src/core.js
@@ -1,13 +1,11 @@
 (function($){
-  function Editor(display, initialText) {
-    // Callbacks
+  function Editor(options) {
     this.afterInsertCallbacks = [];
-
-    this.display = display;
+    this.display = options.display;
     this.buffer = new Buffer(50);
-    this.inputManager = new InputManager();
-    this.insertString(initialText);
-    this.movePoint(-initialText.length);
+    this.inputManager = new options['inputManager'](this);
+    this.insertString(options.initialText);
+    this.movePoint(-options.initialText.length);
   }
 
   Editor.prototype = {
@@ -170,98 +168,15 @@
     }
   };
 
-  function InputManager(editor) {
-    this.bindings = {};
-    this.handleInput = function(e) {
-      var c = String.fromCharCode(e.which);
-      bindings[c](editor);
-    };
-  }
-
-  var EMPTY = 0;
-  var KBD_SHIFT = 1;
-  var KBD_CONTROL = 2;
-  var KBD_ALT = 4;
-
-  var SPECIAL_KEY_STRINGS = {
-     8 : '<BACKSPACE>',
-     9 : '<TAB>',
-    13 : '<ENTER>',
-    27 : '<ESC>',
-    32 : '<SPACE>',
-    37 : '<LEFT>',
-    38 : '<UP>',
-    39 : '<RIGHT>',
-    40 : '<DOWN>',
-    46 : '<DELETE>'
-  };
-  for (var i=1; i<124; i++) {
-    SPECIAL_KEY_STRINGS[111 + i] = '<F' + i + '>';
-  }
-
-  function keyStringFromCode(charCode) {
-    if (typeof SPECIAL_KEY_STRINGS[charCode] !== 'undefined') {
-      return SPECIAL_KEY_STRINGS[charCode];
-    } else {
-      return String.fromCharCode(charCode);
-    }
-  }
-
-  InputManager.prototype = {
-    bindKey : function(command, f) {
-      var keys = command.split('-');
-      var modifiers = EMPTY;
-      while (keys.length > 1) {
-        var mod = keys.shift();
-        if (mod == 'S') {
-          modifiers = modifiers | KBD_SHIFT;
-        }
-        if (mod == 'C') {
-          modifiers = modifiers | KBD_CONTROL;
-        }
-        if (mod == 'A') {
-          modifiers = modifiers | KBD_ALT;
-        }
-      }
-
-      // There's probably a better way to do this
-      var keyString = keys[0];
-      if (typeof SPECIAL_KEY_STRINGS[keyString] === "undefined") {
-        keyString = keyString.toUpperCase();
-      }
-
-      this.bindings[keyString + modifiers] = f;
-    },
-
-    handler : function(editor) {
-      var bindings = this.bindings;
-      return function(e) {
-        var keyString = keyStringFromCode(e.which);
-        var modifiers = EMPTY;
-        if (e.shiftKey) {
-          modifiers = modifiers | KBD_SHIFT;
-        }
-        if (e.ctrlKey) {
-          modifiers = modifiers | KBD_CONTROL;
-        }
-        if (e.altKey) {
-          modifiers = modifiers | KBD_ALT;
-        }
-
-        if (typeof bindings[keyString + modifiers] !== "undefined") {
-          e.preventDefault();
-          return bindings[keyString + modifiers](editor, e);
-        } else {
-          e.preventDefault();
-          return true;
-        }
-      };
-    }
-  };
-
   function NullDisplay() {}
   NullDisplay.prototype.paint = function(editor) {
     // noop display
+  };
+
+  function NullInputManager() {}
+  NullInputManager.prototype = {
+    bindKey : function(command, f) {},
+    handler : function(editor) {}
   };
 
   $.fn.s2e = function(opts) {
@@ -282,7 +197,9 @@
       var options = $.extend({}, $.fn.s2e.defaults, opts);
 
       var d = new options['display'](canvas);
-      var e = new Editor(d, options.initialText);
+      var e = new Editor({ display: d,
+                           initialText: options.initialText,
+                           inputManager: options.inputManager });
       textarea.val(options.initialText);
 
       $.each(options.keybindings, function(key, binding){
@@ -343,9 +260,10 @@
 
   // Defaults
   $.fn.s2e.defaults = {
-    initialText : "",
-    keybindings : defaultKeys,
-    display     : NullDisplay
+    initialText  : "",
+    keybindings  : defaultKeys,
+    display      : NullDisplay,
+    inputManager : NullInputManager
   };
 
   // Export extension points
---
+++ src/input.js
@@ -0,0 +1,88 @@
+(function($){
+  function InputManager(editor) {
+    this.bindings = {};
+  }
+
+  var EMPTY = 0;
+  var KBD_SHIFT = 1;
+  var KBD_CONTROL = 2;
+  var KBD_ALT = 4;
+
+  var SPECIAL_KEY_STRINGS = {
+     8 : '<BACKSPACE>',
+     9 : '<TAB>',
+    13 : '<ENTER>',
+    27 : '<ESC>',
+    32 : '<SPACE>',
+    37 : '<LEFT>',
+    38 : '<UP>',
+    39 : '<RIGHT>',
+    40 : '<DOWN>',
+    46 : '<DELETE>'
+  };
+  for (var i=1; i<124; i++) {
+    SPECIAL_KEY_STRINGS[111 + i] = '<F' + i + '>';
+  }
+
+  function keyStringFromCode(charCode) {
+    if (typeof SPECIAL_KEY_STRINGS[charCode] !== 'undefined') {
+      return SPECIAL_KEY_STRINGS[charCode];
+    } else {
+      return String.fromCharCode(charCode);
+    }
+  }
+
+  InputManager.prototype = {
+    bindKey : function(command, f) {
+      var keys = command.split('-');
+      var modifiers = EMPTY;
+      while (keys.length > 1) {
+        var mod = keys.shift();
+        if (mod == 'S') {
+          modifiers = modifiers | KBD_SHIFT;
+        }
+        if (mod == 'C') {
+          modifiers = modifiers | KBD_CONTROL;
+        }
+        if (mod == 'A') {
+          modifiers = modifiers | KBD_ALT;
+        }
+      }
+
+      // There's probably a better way to do this
+      var keyString = keys[0];
+      if (typeof SPECIAL_KEY_STRINGS[keyString] === "undefined") {
+        keyString = keyString.toUpperCase();
+      }
+
+      this.bindings[keyString + modifiers] = f;
+    },
+
+    handler : function(editor) {
+      var bindings = this.bindings;
+      return function(e) {
+        var keyString = keyStringFromCode(e.which);
+        var modifiers = EMPTY;
+        if (e.shiftKey) {
+          modifiers = modifiers | KBD_SHIFT;
+        }
+        if (e.ctrlKey) {
+          modifiers = modifiers | KBD_CONTROL;
+        }
+        if (e.altKey) {
+          modifiers = modifiers | KBD_ALT;
+        }
+
+        if (typeof bindings[keyString + modifiers] !== "undefined") {
+          e.preventDefault();
+          return bindings[keyString + modifiers](editor, e);
+        } else {
+          e.preventDefault();
+          return true;
+        }
+      };
+    }
+  };
+
+  $.fn.s2e.defaults.inputManager = InputManager;
+})(jQuery);
