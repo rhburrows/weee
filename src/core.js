cd0bb4b5a952c25009f9a67ae24e96d120a66120
Ryan Burrows <rhburrows@gmail.com>
2011-06-13 23:38:00 -0700

    Continue breaking out "non-core" functionality
    The actual display implementation is removed from the core library so
    (in theory) a display that was built upon a different technology (in
    case canvas doesn't work out for this) can be swapped in.

--- src/core.js
+++ src/core.js
@@ -259,144 +259,9 @@
     }
   };
 
-  function Face(options) {
-    this.style      = options['style']       || 'normal';
-    this.variant    = options['variant']     || 'normal';
-    this.weight     = options['weight']      || 'normal';
-    this.size       = options['size']        || 'medium';
-    this.lineHeight = options['line-height'] || 'normal';
-    this.family     = options['family']      || 'monospace';
-    this.color      = options['color']       || 'black';
-  }
-
-  Face.prototype = {
-    fontString : function() {
-      return this.style + ' ' +
-        this.variant + ' ' +
-        this.weight + ' ' +
-        this.size + '/' +
-        this.lineHeight + ' ' +
-        this.family + ' ';
-    }
-  };
-
-  function Display(canvas) {
-    this.canvas = canvas;
-    this.context = canvas.getContext('2d');
-    this.padding = 20;
-    this.faces = [];
-    this.defaultFace = new Face({ family : 'Monaco', size : 14 });
-    this.applyFace(this.defaultFace);
-  }
-
-  Display.prototype = {
-    paint : function(editor) {
-      this.clear();
-
-      var col = 0, row = 0;
-      var currentLineCount = 0;
-      var contents = editor.contents();
-      for (var i=0; i<contents.length; i++) {
-        var c = contents.charAt(i);
-
-        if (this.faceForPosition(i) != this.currentFace) {
-          this.applyFace(this.faceForPosition(i));
-        }
-
-        if (c == '\n') {
-          if (i == editor.pointPosition()) {
-            this.paintCursor(col, row);
-          }
-
-          col = 0;
-          row++;
-          currentLineCount = 0;
-        } else {
-          this.paintCharacter(c, col, row);
-
-          if (i == editor.pointPosition()) {
-            this.paintCursor(col, row);
-          }
-
-          col++;
-          currentLineCount++;
-        
-          if (currentLineCount == this.lineLength) {
-            this.paintLineWrappingMarker(col, row);
-            col = 0;
-            row++;
-            currentLineCount = 0;
-          }
-        }
-      }
-    },
-
-    paintCharacter : function(character, col, row) {
-      var pixelX = this.columnToX(col);
-      var pixelY = this.rowToY(row);
-      this.context.fillText(character, pixelX, pixelY);
-    },
-
-    paintCursor : function(col, row) {
-      var pixelX = this.columnToX(col);
-      var pixelY = this.rowToY(row);
-      var face = this.currentFace;
-
-      this.context.fillStyle = 'red';
-      this.context.fillText('|', pixelX - (this.charWidth / 2), pixelY);
-      
-      this.applyFace(face);
-    },
-
-    paintLineWrappingMarker : function(col, row) {
-      var pixelX = this.columnToX(col);
-      var pixelY = this.rowToY(row);
-      var face = this.currentFace;
-
-      this.context.fillStyle = 'grey';
-      this.context.fillRect(pixelX, pixelY-(this.lineHeight /2), 5, 5);
-
-      this.applyFace(face);
-    },
-
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-
-    width : function() {
-      return $(this.canvas).width();
-    },
-
-    faceForPosition : function(position) {
-      for (var i=0; i<this.faces.length; i++) {
-        var faceApplication = this.faces[i];
-        if (faceApplication[0] <= position && position <= faceApplication[1]) {
-          return faceApplication[2];
-        }
-      }
-      return this.defaultFace;
-    },
-
-    setFace : function(from, to, face) {
-      this.faces.push([from, to, face]);
-    },
-
-    applyFace : function(face) {
-      this.context.fillStyle = face.color;
-      this.charWidth = face.size * 0.8;
-      this.lineHeight = face.size * 1.6;
-      this.context.font = face.fontString();
-      this.lineLength = Math.floor((this.width() - 2 * this.padding) / this.charWidth);
-      this.currentFace = face;
-    },
-
-    columnToX : function(col) {
-      return this.padding + (col * this.charWidth);
-    },
-
-    rowToY : function(row) {
-      return this.padding + (row * this.lineHeight);
-    }
+  function NullDisplay() {}
+  NullDisplay.prototype.paint = function(editor) {
+    // noop display
   };
 
   $.fn.s2e = function(opts) {
@@ -416,10 +281,7 @@
 
       var options = $.extend({}, $.fn.s2e.defaults, opts);
 
-      var d = new Display(canvas);
-      var redFace = new Face({ family: 'Monaco', size: 14 });
-      redFace.color = 'red';
-      d.setFace(10, 20, redFace);
+      var d = new options['display'](canvas);
       var e = new Editor(d, options.initialText);
       textarea.val(options.initialText);
 
@@ -436,7 +298,6 @@
       $(canvas).click(function(){ textarea.focus(); });
     });
   };
-
 
   function insertF(c) {
     return function(e, ev){ e.insertChar(c); };
@@ -483,7 +344,8 @@
   // Defaults
   $.fn.s2e.defaults = {
     initialText : "",
-    keybindings : defaultKeys
+    keybindings : defaultKeys,
+    display     : NullDisplay
   };
 
   // Export extension points
