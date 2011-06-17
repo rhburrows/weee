99793def25f21b02b712f1f7842a0fd746690f0b
Ryan Burrows <rhburrows@gmail.com>
2011-06-17 07:22:57 -0700

    Test the bubbling of events
    Events triggered within the application should bubble out through the
    targetted textarea so that users of the library can bind additional
    functionality to them. Write tests to verify this behavior so it isn't
    accidentally broken later

--- test/coreTest.js
+++ test/coreTest.js
@@ -23,3 +23,50 @@ test("click", function(){
   equals($('#editor:focus').size(), 1,
          "The textarea should be focused after clicking on the canvas");
 });
+
+test("s2e:click is sent to the textarea", function(){
+  textarea.bind('s2e:click', function(){
+    ok("s2e:click called!");
+  });
+
+  expect(1);
+  $('canvas').click();
+});
+
+test("s2e:mousedown is sent to the textarea", function(){
+  textarea.bind('s2e:mousedown', function(){
+    ok("s2e:mousedown called!");
+  });
+
+  expect(1);
+  $('canvas').mousedown();
+});
+
+test("s2e:mouseup is sent to the textarea", function(){
+  textarea.bind('s2e:mouseup', function(){
+    ok("s2e:mouseup called!");
+  });
+
+  expect(1);
+  $('canvas').mouseup();
+});
+
+test("s2e:contentsUpdate is sent to the textarea", function(){
+  textarea.bind('s2e:contentsUpdate', function(){
+    ok("s2e:contentsUpdate called!");
+  });
+
+  expect(1);
+  editor.insertString("Hello!");
+});
+
+test("s2e:movePoint is sent to the textarea", function(){
+  editor.insertString("Hello!");
+
+  textarea.bind('s2e:movePoint', function(){
+    ok("s2e:contentsUpdate called!");
+  });
+
+  expect(1);
+  editor.movePoint(-2);
+});
